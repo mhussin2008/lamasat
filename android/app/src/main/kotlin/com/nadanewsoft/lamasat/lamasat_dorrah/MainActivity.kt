ef7bcd6433b35cbf159c644d67b8975691d83ed3
mohamed hussin <65326535+mhussin2008@users.noreply.github.com>
2023-03-18 07:59:43 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/nadanewsoft/lamasat/lamasat_dorrah/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nadanewsoft.lamasat.lamasat_dorrah
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/myHomePage.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'privacy_text.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+//--------------------------------------------------------------------------
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 1;
+  String jpg = '001';
+  String zeros = '';
+  int temp = 0;
+  List<String> myIndex = [''];
+  List<int> pageNum = [0];
+  bool privacy = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    myIndex = [
+      'مقدمة المؤلف',
+          'مدخل لدراسة الدرة',
+          'الأوجه المقدمة في الأداء',
+          'مقدمة النظم',
+          'باب البسملة وأم القرآن',
+          'الإدغام الكبير',
+          'هاء الكناية',
+          'المد والقصر',
+          'الهمزتان من كلمة',
+          'الاستفهام المكرر',
+          'الهمزتان من كلمتبن',
+          'الهمز المفرد',
+          'النقل والسكت والوقف على الهمز',
+          'الإدغام الصفير',
+          'النون الساكنة والتنوين',
+          'الفتح والإمالة',
+          'الراء ات واللامات والوقف على المرسوم',
+          'ياءات الإضافة',
+          'الياءات الزوائد',
+          'فرش سورة البقرة',
+          'فرش سورة آل عمران',
+          'فرش سورة النساء',
+          'فرش سورة المائدة',
+          'المراجع'
+
+    ];
+    pageNum = [
+      7,
+      8,
+      19,
+      25,
+      48,
+      68,
+      82,
+      95,
+      98,
+      11,
+      118,
+      124,
+      149,
+      164,
+      182,
+      184,
+      195,
+      213,
+      226,
+      249,
+      337,
+      336,
+      377,
+      388
+    ];
+  }
+
+  void _makeFileName() {
+    zeros = '';
+    temp = 3 - _counter.toString().length;
+    for (int i = 0; i < temp; i++) {
+      zeros = zeros + '0';
+    }
+    jpg = zeros + _counter.toString();
+    //print(jpg);
+  }
+
+  void _incrementCounter() {
+    if (_counter >= 390) {
+      return;
+    }
+    setState(() {
+      _counter++;
+      _makeFileName();
+    });
+  }
+
+  void _decrementCounter() {
+    if (_counter < 2) {
+      return;
+    }
+    setState(() {
+      _counter--;
+      _makeFileName();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+            'كتاب اللمسات الندية فى الدرة المضية',
+        textAlign: TextAlign.right
+          ,
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  privacy = !privacy;
+                  print(privacy);
+                });
+
+                //launchUrl(Uri(path:'https://github.com/mhussin2008/mohamed-privacy/blob/main/privacy-policy.md'));
+              },
+              icon: Icon(Icons.privacy_tip))
+        ],
+      ),
+      drawer: SafeArea(
+        child: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView.builder(
+                padding: const EdgeInsets.all(0),
+                itemCount: myIndex.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                      title: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Row(
+                          children: [
+                            Text(' ${pageNum[index]}'),
+                            Expanded(
+                                child: SizedBox(
+                                    child: Text(
+                              myIndex[index],
+                              textAlign: TextAlign.right,
+                            ))),
+                            //
+                          ],
+                        ),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          _counter = pageNum[index] + 1;
+                          privacy = true;
+                          _makeFileName();
+                        });
+                        Navigator.pop(context);
+                      });
+                })),
+      ),
+      // appBar: AppBar(title: Text(widget.title), ),
+      body: privacy
+          ? Dismissible(
+              key: ValueKey(_counter),
+              resizeDuration: null,
+              onDismissed: (DismissDirection direction) {
+                if (direction == DismissDirection.endToStart) {
+                  _decrementCounter();
+                } else {
+                  _incrementCounter();
+                }
+              },
+              child: Center(child: Image.asset('assets/pages/lamasat-page-' + jpg + '.jpg')),
+            )
+          : SingleChildScrollView(child: Text(privacy_text)),
+    );
+    // This trailing comma makes auto-formatting nicer for build methods.
+  }
+}
