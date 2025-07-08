1b85ed5df54c0ae2028c0f0d052deb876cac8c9e
Programming <mhussin2008@yahoo.com>
2025-07-08 15:31:20 +0300

    google play 1 added keystore

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -5,7 +5,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/services.dart';
 import 'privacy_text.dart';
-import 'videoList.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -18,18 +17,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   String url = 'https://www.youtube.com/playlist?list=PLo_bzJr12Ms5Ikr5mRUZHfey4Jg-HsOcv';
-  String Privacyurl='https://www.freeprivacypolicy.com/live/5bc098fb-a590-462b-8df3-d0d0e7c60597';
+  String Privacyurl='https://github.com/mhussin2008/mohamed-privacy/blob/main/privacy-policy.md';
   late Uri myUri=Uri.parse(url);
   late Uri myPrivacyUri=Uri.parse(Privacyurl);
   int _counter = 1;
   String jpg = '001';
   String zeros = '';
   int temp = 0;
-  //List<String> myIndex = [''];
-  //List<String> myVideoIndex = [''];
-  //String myVideoListUrl='https://www.youtube.com/watch?v=aOZYPsJLkU0&list=PLo_bzJr12Ms5Ikr5mRUZHfey4Jg-HsOcv&index=';
-
-
+  List<String> myIndex = [''];
+  List<int> pageNum = [0];
   bool privacy = true;
 
   @override
@@ -37,14 +33,105 @@ class _MyHomePageState extends State<MyHomePage> {
     // TODO: implement initState
     super.initState();
 
-    // myVideoIndex=[
-    // 'https://www.youtube.com/watch?v=aOZYPsJLkU0&list=PLo_bzJr12Ms5Ikr5mRUZHfey4Jg-HsOcv&index=1',
-    //   'https://www.youtube.com/watch?v=aOZYPsJLkU0&list=PLo_bzJr12Ms5Ikr5mRUZHfey4Jg-HsOcv&index=2'
-    //
-    //
-    // ];
+    myIndex = [
+      'تقريظ أ. ياسر السمرى',
+          'تقريظ د. محمد فؤاد عبدالمجيد',
+          'تقريظ أ.د. عبدالقيوم السندى',
+          'مقدمة المؤلف',
+          'مدخل لدراسة الدرة',
+          'فصل فى الأوجه المقدمة في الأداء',
+          'فصل فيما زادته الدرة على الشاطبية',
+          'مقدمة النظم',
+          'باب البسملة وأم القرآن',
+          'الإدغام الكبير',
+          'هاء الكناية',
+          'المد والقصر',
+          'الهمزتان من كلمة',
+          'الاستفهام المكرر',
+          'الهمزتان من كلمتبن',
+          'الهمز المفرد',
+          'النقل والسكت والوقف على الهمز',
+          'الإدغام الصفير',
+          'النون الساكنة والتنوين',
+          'الفتح والإمالة',
+          'الراءات واللامات والوقف على المرسوم',
+          'ياءات الإضافة',
+          'الياءات الزوائد',
+          'باب فرش الحروف :فرش سورة البقرة',
+          'فرش سورة آل عمران',
+          'فرش سورة النساء',
+          'فرش سورة المائدة',
+          'سورة الأنعام',
+          'سورة الأعراف والأنفال',
+          'سورة التوبة ويونس وهود عليهما السلام',
+          'سورة يوسف-عليه السلام- والرعد',
+          'ومن سورة إبراهيم-عليه السلام-إلى سورة الكهف',
+          'سورة الكهف',
+          'ومن سورة مريم-عليها السلام-إلى سورة الفرقان',
+          'ومن سورة الفرقان إلى سورة الروم',
+          'سورة الروم ولقمان -عليه السلام- والسجدة',
+          'سورة الأحزاب وسبأو فاطر',
+          'سورة يس-عليه السلام -والصافات',
+          'ومن سورة ص إلى سورة الأحقاف',
+          'ومن سورة الأحقاف إلى سورة الرحمن',
+          'ومن سورة الرحمن إلى سورة الإمتحان',
+          'ومن سورة الإمتحان إلى سورة الجن',
+          'ومن سورة الجن إلى سورة المرسلات',
+          'ومن سورة المرسلات إلى سورة الغاشية',
+          'ومن سورة الغاشية إلى آخر القرآن',
+          'المراجع'
 
 
+    ];
+    pageNum = [
+      9,
+      10,
+      11,
+      13,
+      14,
+      24,
+      30,
+      58,
+      80,
+      100,
+      114,
+      126,
+      129,
+      141,
+      148,
+      154,
+      179,
+      194,
+      210,
+      212,
+      222,
+      240,
+      253,
+      275,
+      356,
+      378,
+      395,
+      405,
+      430,
+      457,
+      486,
+      491,
+      512,
+      524,
+      559,
+      576,
+      584,
+      599,
+      614,
+      635,
+      647,
+      657,
+      663,
+      673,
+      683,
+      697
+
+    ];
   }
 
   void _makeFileName() {
@@ -58,7 +145,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _incrementCounter() {
-    if (_counter >= 550) {
+    if (_counter >= 699) {
       return;
     }
     setState(() {
@@ -144,7 +231,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
             IconButton(
                 onPressed: () {
-                  _launchUrl(myUri);
+                  _launchUrl();
                   //launchUrl(Uri(path:'https://github.com/mhussin2008/mohamed-privacy/blob/main/privacy-policy.md'));
                 },
                 icon:Image.asset('assets/icons/ypf.png')
@@ -162,7 +249,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 itemCount: myIndex.length,
                 itemBuilder: (BuildContext context, int index) {
                   return Container(
-                    height: 50,
+                    height: 40,
                     child: ListTile(
                         title: Padding(
                           padding: const EdgeInsets.all(2.0),
@@ -171,25 +258,11 @@ class _MyHomePageState extends State<MyHomePage> {
                               Text(' ${pageNum[index]}'),
                               Expanded(
                                   child: SizedBox(
-
                                      child: Text(
-                                       softWrap: true,
                                 myIndex[index],
                                 textAlign: TextAlign.right,
-                              )
-                                  )
-                              ),
-                              index>4 && index<49 ?IconButton(
-                                alignment: Alignment.topRight,
-                                  iconSize: 10,
-                                  onPressed: (){
-
-                               ///// here
-                                String temp='${myVideoList[index-3]}';
-                                print(temp);
-                                _launchUrl(Uri.parse(temp));
-                              }, icon: Image.asset('assets/icons/youtube.png')
-                              ):SizedBox()//
+                              ))),
+                              //
                             ],
                           ),
                         ),
@@ -219,16 +292,16 @@ class _MyHomePageState extends State<MyHomePage> {
               child: InteractiveViewer(
                 maxScale: 5,
 
-                  child: Center(child: Image.asset('assets/lamasat5/lamasat-' + jpg + '.jpg'))),
+                  child: Center(child: Image.asset('assets/lamasat/lamasat-' + jpg +' '+ '.jpg'))),
             )
           : SingleChildScrollView(child: Text(privacy_text)),
     );
     // This trailing comma makes auto-formatting nicer for build methods.
   }
 
-  Future<void> _launchUrl(Uri parse) async {
+  Future<void> _launchUrl() async {
     print('started launching youtube');
-    if (!await launchUrl(parse)) {
+    if (!await launchUrl(myUri)) {
       throw Exception('Could not launch $myUri');
     }
 
